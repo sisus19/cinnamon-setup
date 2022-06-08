461da646086baf3156f1924e8ce2449091d75d70
Sisus <104006944+Sisus19@users.noreply.github.com>
2022-06-08 16:48:25 +0200

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt update
+
+apt install cinnamon tasksel lightdm cinnamon-core fonts-liberation gedit gnome-screenshot vlc cups gedit-plugins htop neofetch gstreamer1.0-libav gstreamer1.0-plugins-ugly libreoffice-writer orca eog system-config-printer zenity gnome-firmware amd64-microcode firmware-amd-graphics firmware-realtek fwupd-amd64-signed r8168-dkms -y
