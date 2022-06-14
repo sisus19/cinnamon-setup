31987bbd1a8a98fb964e9bf74b478417b94af729
sisus <104006944+sisus19@users.noreply.github.com>
2022-06-14 17:28:46 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -16,11 +16,3 @@ cd /usr/share/icons/
 git clone https://github.com/EliverLara/Sweet.git -b nova
 mv /usr/share/icons/Sweet/kde/cursors/Sweet-cursors /usr/share/icons/
 rm -R Sweet/
-
-cd
-mkdir ~/bin && PATH=~/bin:$PATH && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
-
-git config --global user.name "sisus19" && git config --global user.email "silviomasillo@protonmail.com" && git config --global credential.helper "cache --timeout=7200"
-
-
-
