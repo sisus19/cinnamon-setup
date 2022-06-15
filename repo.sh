71e9378c40955de4b99081bac4a9acf7213e21cf
sisus <104006944+sisus19@users.noreply.github.com>
2022-06-15 12:52:28 +0200

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mkdir ~/bin && PATH=~/bin:$PATH && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
+mkdir -p ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+source ~/.profile
 
 git config --global user.name "sisus19" && git config --global user.email "silviomasillo@protonmail.com" && git config --global credential.helper "cache --timeout=7200"
